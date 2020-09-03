5c2f524d19684baaea2208138ef837f39a532d1c
rgrmrts <omar@bricck.co>
2020-09-02 20:05:57 -0400

    substitution

--- substitution-model-for-procedure-application.scm
+++ substitution-model-for-procedure-application.scm
@@ -1,1 +1,11 @@
 ;;; 1.1.5 The Substitution Model for Procedure Application
+
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+
+(f 5)
