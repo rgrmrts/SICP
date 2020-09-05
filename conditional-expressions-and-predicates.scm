1526b42dee14c87967f89384ba5350e6ed301156
rgrmrts <omar@bricck.co>
2020-09-04 23:41:46 -0400

    conditionals and if statement

--- conditional-expressions-and-predicates.scm
+++ conditional-expressions-and-predicates.scm
@@ -1,9 +1,29 @@
 ;;; 1.1.6 Conditional Expressions and Predicates
 
+;; returns the absolute value of x
 (define (abs x)
   (cond ((> x 0) x)
         ((= x 0) 0)
         ((< x 0) (- x))))
+
+(abs 1)
+(abs 0)
+(abs -2)
+
+;; simpler condition for absolute value function
+(define (abs x)
+  (cond ((< x 0) ( - x))
+        (else x)))
+
+(abs 1)
+(abs 0)
+(abs -2)
+
+;; using an if block for the absolute value function
+(define (abs x)
+  (if (< x 0)
+      (- x)
+      x))
 
 (abs 1)
 (abs 0)
