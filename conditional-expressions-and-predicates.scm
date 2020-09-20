95eb18e64d38f431e6c9aba92c7be4ef41ebf6eb
rgrmrts <omar@bricck.co>
2020-09-19 23:02:00 -0400

    1.3

--- conditional-expressions-and-predicates.scm
+++ conditional-expressions-and-predicates.scm
@@ -68,3 +68,13 @@
 
 ;; Exercise 1.2
 (/ (+ 5 (+ 4 (- 2 (- 3 (+ 6 (/ 4 5)))))) (* 3 (* (- 6 2) (- 2 7))))
+
+;; Exercise 1.3
+(define (sum-of-two-largest-squares x y z)
+  (- (+ (* x x)
+        (* y y)
+        (* z z))
+     (* (min x y z)
+        (min x y z))))
+
+(sum-of-two-largest-squares 1 2 3)
